32dcde1a628bacad98a518399c268dbab6fb327c
viniciusbelalva <vinicius.belalva@edu.senai.br>
2026-06-15 15:57:51 -0300

    15/06

---
+++ JS/meu_JS1.js
@@ -0,0 +1,11 @@
+//string
+        var texto = "curso de JavaScript";
+        //number
+        var numInteiro = -7;
+        var numberFracionado = 10.45;
+        //boolean
+        var teste = true; //true/false
+
+        alert(texto);
+        document.write(texto);
+        console.log(texto);
